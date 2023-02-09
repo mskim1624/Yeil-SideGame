a0af938c0d435240cfae324753315d6e2a9572b5
mskim1624 <mskim1624@naver.com>
2023-02-09 12:50:54 +0900

    0209-4-1

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -9,6 +9,13 @@ public class CameraManager : MonoBehaviour
     public float topLimit = 0.0f;
     public float bottomLimit = 0.0f;
 
+    public GameObject subBackScreen;
+
+    public bool isForceScrollX = false;     // x축 장제 스크롤 플래그
+    public float forceScrollSpeedX = 0.5f;  // 1초간 움직일 x의 거리
+    public bool isForceScrollY = false;     // y축 강제 스크롤 플래그
+    public float forceScrollSpeedY = 0.5f;  // 1초간 움직일 y의 거리 
+
     // Update is called once per frame
     void Update()
     {
@@ -23,6 +30,11 @@ public class CameraManager : MonoBehaviour
         float y = player.transform.position.y;
         float z = this.transform.position.z;
 
+        if (isForceScrollX)
+        {
+            x = transform.position.x + (forceScrollSpeedX * Time.deltaTime);
+        }
+
         if (x < leftLimit)
         {
             x = leftLimit;
@@ -30,6 +42,11 @@ public class CameraManager : MonoBehaviour
         else if(x > rightLimt)
         {
             x = rightLimt;
+        }
+
+        if (isForceScrollY)
+        {
+            y = transform.position.y + (forceScrollSpeedY * Time.deltaTime);
         }
 
         if(y < bottomLimit)
@@ -43,5 +60,14 @@ public class CameraManager : MonoBehaviour
 
         Vector3 v3 = new Vector3(x, y, z);
         transform.position = v3;
+
+        if (subBackScreen != null)
+        {
+            y = subBackScreen.transform.position.y;
+            z = subBackScreen.transform.position.z;
+
+            Vector3 vSub = new Vector3(x / 2.0f, y, z);
+            subBackScreen.transform.position = vSub;
+        }
     }
 }
